772cd3a2d9a50f52ade9e268262f4f9d860e6dcc
Ben0xA <web@ben0xa.com>
2015-06-09 09:27:32 -0500

    Initial Commit
    Initial Commit

---
+++ AwesomerShell/AwesomerShell/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+
+namespace AwesomerShell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("aps> ");
+            String cmd = Console.ReadLine();
+
+            Runspace rs = RunspaceFactory.CreateRunspace();
+            rs.Open();
+
+            PowerShell ps = PowerShell.Create();
+            ps.Runspace = rs;
+            ps.AddScript(cmd);
+
+            Collection<PSObject> output = ps.Invoke();
+            if (output != null)
+            {
+                foreach (PSObject rtnItem in output)
+                {
+                    Console.WriteLine(rtnItem.ToString());
+                }
+            }
+            rs.Close();
+            Console.Write("Press any key to exit.");
+            Console.ReadLine();
+        }
+    }
+}
